38fc4e94fdb55123db06d53ba432fd7e1f4e4fd9
Aniket Dubey <149755862+Aniket0304@users.noreply.github.com>
2024-12-06 07:32:20 +0000

    datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // kisi bhi code ko newer version me karne ke liye use kiya jaata hai
+//alert(3+3) // ye nodejs me kaam nahi karega sirf browser me kaam karega 
+
+// datatypes
+
+/*
+numbers => 2 to the power 53
+big int 
+string 
+boolean 
+null 
+undefined
+symbol => for uniqueness
+
+object 
+*/
+console.log(typeof null) // iska type object hai jo ki controversial cheez hai 
+console.log(typeof undefined) // iska type undefined hi hai 
