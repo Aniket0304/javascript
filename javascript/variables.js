aedd9ef52430addf0ded5a6b6787cb31faf28586
Aniket Dubey <149755862+Aniket0304@users.noreply.github.com>
2024-12-06 06:55:19 +0000

    new

---
+++ javascript/variables.js
@@ -0,0 +1,6 @@
+const accountId = 11222 // cannot change const value permanently fixed 
+let accountEmail = "Aniketdubey123@gmail.com" // variable that can be changed and not used 
+var accountPass = "12455" // don't use this var word because it causes some problem in functional scope
+accountCity = "Barabanki" // this is also possible but bad practice 
+
+console.table([accountId, accountEmail , accountPass , accountCity])
