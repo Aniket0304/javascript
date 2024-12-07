cfb915de16d812aa18d81149720e6207514d8e86
Aniket Dubey <149755862+Aniket0304@users.noreply.github.com>
2024-12-07 15:30:02 +0000

    conversion

---
+++ javascript/conversion.js
@@ -0,0 +1,16 @@
+let score = "33abc"
+
+/*console.log(typeof score)
+let numbersystem = Number(score)
+console.log(typeof numbersystem) // dikkat ye hai isme ki isne convert to kiya hai score ko lekin wo sahi tarah se convert nahi 
+// hua hai kyunki usme alphabets bhi hai toh ye number type me de toh raha hai lekin ye sahi nahi hai
+console.log(numbersystem) // ye isse pata chal jaayega ki kitta sahi hai kitta galat (NaN de raha hai)
+*/
+
+let somenumber = 33 
+let value = String(somenumber)
+console.log(value);
+console.log(typeof value);
+
+
+
