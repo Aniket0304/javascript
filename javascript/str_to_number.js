4d48d5323e355e88c25875e01e4cbdcca67625ae
Aniket Dubey <149755862+Aniket0304@users.noreply.github.com>
2024-12-07 16:02:25 +0000

    str_to_number

---
+++ javascript/str_to_number.js
@@ -0,0 +1,7 @@
+console.log(1 + "2"); // same isme bhi hua hai 
+console.log("2" + 1); // aur iska bhi yahi rule hai 
+console.log(2 + 1 + "2"); // isne phle 2 + 1 kiya hai phir jaise isse ek string dikhi waise concatentae kar diya hai 
+console.log("2" + 1 + 2); // yaha pe phle hi string dikh gayi toh sabko hi concatenate kiya hai 
+
+
+
