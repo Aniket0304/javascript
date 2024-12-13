bdaddc40cb8ed74802a77453a54a26ac556bc535
Aniket Dubey <149755862+Aniket0304@users.noreply.github.com>
2024-12-13 06:29:29 +0000

    summary

---
+++ javascript/basic_summary.js
@@ -0,0 +1,21 @@
+// Primitive and Non - Primitive data types
+
+// 7 types : String , number , boolean , BigINT , Symbol , null , undefined 
+// they all are pass by values 
+// the changes occurs in their copied data not in the original one 
+
+
+// NON - PRIMITIVE  
+
+// Arrays , Objects , functions 
+
+const heros = ["bahubali" , "masakali" , "chain kuli ki main kuli"];
+const myobj = {
+    name : "Aniket",
+    age : 20,
+}
+
+const myfunction = function(){
+    console.log("Hello world");
+    
+}
