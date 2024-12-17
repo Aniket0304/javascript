3d003f1859110b1331abd761d6c0f7eb58a17be9
Aniket Dubey <149755862+Aniket0304@users.noreply.github.com>
2024-12-17 13:56:23 +0000

    maths

---
+++ javascript/numbers_maths.js
@@ -0,0 +1,16 @@
+const score = 100
+const balance = new Number(400) // ye cheez guarantee karti hai ki number hi hoga 
+
+
+console.log(score);
+console.log(balance.toString()); 
+
+console.log(score.toFixed(1)); // this will fix give the value upto how many decimal places you want
+
+const value = 123.998989
+console.log(value.toPrecision(7)); // upto how many significant figures you want precision and will give the output in string 
+
+
+
+
+
