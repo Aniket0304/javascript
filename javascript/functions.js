5b57620df382b9bec6ed9fc5a4d0fd1f7a2da69b
Aniket Dubey <149755862+Aniket0304@users.noreply.github.com>
2025-01-27 18:49:39 +0000

    fuctions

---
+++ javascript/functions.js
@@ -0,0 +1,8 @@
+// sab kaam same as c++ function ka bas isme thoda syntax change hai
+function add(number1 , number2){
+    return number1 + number2
+}
+console.log(add(3,4))
+
+// return ke baad koi bhi statement kaam nahi karega 
+// aur baaki sab same hi  hai calling aur sab kuch , parameters bhi sab same hi hai 
